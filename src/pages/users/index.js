5efcdc7135a6f3ee6ac10d321c6e3eb0092bb04a
THorne93 <135068951+THorne93@users.noreply.github.com>
2025-05-29 15:18:49 +0200

    b

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,25 +1,27 @@
 import prisma from '../../../lib/prisma';
 
 export default async function UsersPage() {
-  const users = await prisma.users.findMany({
-    include: { reviews: true },
-  });
-
-  return (
-    <main>
-      <h1>Users</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            {user.username} ({user.email})
-            <ul>
-              {user.reviews.map(review => (
-                <li key={review.id}>{review.title}</li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+  try {
+    const users = await prisma.users.findMany({ include: { reviews: true } });
+    return (
+      <main>
+        <h1>Users</h1>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              {user.username} ({user.email})
+              <ul>
+                {user.reviews.map(review => (
+                  <li key={review.id}>{review.title}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return <div>Failed to load users</div>;
+  }
 }
