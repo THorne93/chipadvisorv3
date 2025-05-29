f31971c3c18640ef3782c1b0e31972bac0ac6dda
THorne93 <135068951+THorne93@users.noreply.github.com>
2025-05-29 12:03:24 +0200

    E

--- src/app/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1>Dashboard Home</h1>
-      <p>Welcome to your dashboard!</p>
-      {/* Your dashboard content here */}
-    </div>
-  );
-}
